1a7df3caeeac690cd3ee40e257514f1e588ab9df
Kvn91 <k.dargere@gmail.com>
2024-01-04 11:28:10 +0100

    Add timestamp to Scenario model

--- models/Scenario.js
+++ models/Scenario.js
@@ -1,24 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const scenarioSchema = new Schema({
-    id: {
-        type: Number,
-        required: true
+const scenarioSchema = new Schema(
+    {
+        id: {
+            type: Number,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            requried: false,
+            ref: "User"
+        }
     },
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        requried: false,
-        ref: "User"
+    {
+        timestamps: true
     }
-});
+);
 
 module.exports = mongoose.model('Scenario', scenarioSchema);
