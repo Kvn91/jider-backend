87f2c4dfe0d2fff2c28aa8895e36b4af93298d6d
Kvn91 <k.dargere@gmail.com>
2024-01-05 17:06:40 +0100

    Fix API routing

--- routes/api/scenarios.js
+++ routes/api/scenarios.js
@@ -8,13 +8,13 @@ router.route('/')
     .get(scenariosController.getAllScenarios)
     // .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), scenariosController.createScenario)
     // .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), scenariosController.updateScenario)
-    // .delete(verifyRoles(ROLES_LIST.Admin), scenariosController.deleteScenario)
     .post(scenariosController.createScenario)
     .put(scenariosController.updateScenario)
 ;
 
 router.route('/:id')
     .get(scenariosController.getScenario)
+    // .delete(verifyRoles(ROLES_LIST.Admin), scenariosController.deleteScenario)
     .delete(scenariosController.deleteScenario)
 ;
 
